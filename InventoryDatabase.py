63510e846fbec5de56664cc4cecb22cba0ccf656
Leonel2005 <leovillanueva1110@gmail.com>
2025-09-29 17:59:31 -0700

    Was able to display the csv file on a popup

--- InventoryDatabase.py
+++ InventoryDatabase.py
@@ -1,8 +1,26 @@
 import csv
+import tkinter
+from tkinter.constants import *
+from tkinter import *
 
-with open('JJInventoryDatabase - Sheet1.csv', 'r') as file:
-    reader = csv.reader(file)  # For comma-separated values
-        # For tab-separated values, use: reader = csv.reader(file, delimiter='\t')
-        
+tk = tkinter.Tk()
+tk.geometry("600x600")
+frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=10)
+frame.pack(fill=BOTH, expand=1)
+
+csv_content = ""
+with open('JJInventoryDatabase - Sheet1.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)  # For comma-separated values
+        # For tab-separated values, use: reader = csv.reader(file, delimiter='\t')   
     for row in reader:
-        print(row)
+        csv_content += "\t".join(row) + "\n"
+        
+text_widget = tkinter.Text(frame, wrap="none")
+text_widget.insert(tkinter.END, csv_content)
+text_widget.pack(fill=BOTH, expand=1)        
+        
+button = tkinter.Button(frame, text="Exit", command=tk.destroy)
+button.pack(side=RIGHT)
+tk.mainloop()
+ 
+ 
