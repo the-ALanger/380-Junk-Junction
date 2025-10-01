85cbaa483de8b52de416febfc63df11dc9e0f63f
SarkisNaz <sarkisn03@gmail.com>
2025-10-01 14:48:13 -0700

    text box

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,14 +2,43 @@
 import tkinter
 from tkinter.constants import *
 from tkinter import *
-tk = tkinter.Tk()
+
+homepage = tkinter.Tk()
+homepage.geometry("800x600")
+frame = tkinter.Frame(homepage, relief=RIDGE, borderwidth=10)
+frame.pack(fill=BOTH, expand=1)
+
+homepage.title('Junk Junction')
+
+title = tkinter.Label(frame, text = 'Junk Junction', bg="darkred", font = ("Times New Roman", 12))
+title.pack(fill=X, side=TOP, expand=1)
+
+"""
+Label(homepage, text='First Name').grid(row=1)
+Label(homepage, text='Last Name').grid(row=1)
+e1 = Entry(homepage)
+e2 = Entry(homepage)
+e1.grid(row=0, column=0)
+e2.grid(row=1, column=0)
+"""
+
+button = tkinter.Button(frame, text='Return to Desktop', width=15, command=homepage.destroy)
+button.pack()
+"""
+label = tkinter.Label(frame, text="Junk Junction")
+label.pack(fill=X, expand=2, side=TOP)
+
+button = tkinter.Button(frame, text="Exit", command=tk.destroy)
+button.pack(side=RIGHT)
+"""
+homepage.mainloop()
+
+
+"""tk = tkinter.Tk()
 tk.geometry("600x600")
 frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=10)
 frame.pack(fill=BOTH, expand=1)
+
 label = tkinter.Label(frame, text="Hello, World")
-label2 = tkinter.Label(frame, text="Sarkis was here", bg = "lightblue", font = ("Arial", 16))
 label.pack(fill=X, expand=1)
-label2.pack(fill=X, expand=1)
-button = tkinter.Button(frame, text="Exit", command=tk.destroy)
-button.pack(side=RIGHT)
-tk.mainloop()
+"""
