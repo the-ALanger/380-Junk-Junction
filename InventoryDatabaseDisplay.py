156841e9fef365838805b2ac5bfb7d2e729905ef
Leonel2005 <leovillanueva1110@gmail.com>
2025-10-01 18:14:50 -0700

    added inventory display

---
+++ InventoryDatabaseDisplay.py
@@ -0,0 +1,21 @@
+import csv
+import tkinter
+from tkinter.constants import *
+from tkinter import *
+
+tk = tkinter.Tk()
+tk.geometry("600x600")
+frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=10)
+frame.pack(fill=BOTH, expand=1)
+        
+Inv_Data = InventoryDatabase()
+        
+text_widget = tkinter.Text(frame, wrap="none")
+text_widget.insert(tkinter.END, Inv_Data.csv_w_label)
+text_widget.pack(fill=BOTH, expand=1)        
+        
+button = tkinter.Button(frame, text="Exit", command=tk.destroy)
+button.pack(side=RIGHT)
+tk.mainloop()
+ 
+ 
