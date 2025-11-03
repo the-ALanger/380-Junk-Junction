f689bc0c03d182f83672bcb3e916f6477833cacf
IanFlack22 <ian.flack.694@my.csun.edu>
2025-11-03 14:15:20 -0800

    Created UserDatabase methods, added comments to main and added User tests in main

--- Main.py
+++ Main.py
@@ -1,16 +1,39 @@
 from InventoryDatabase import InventoryDatabase
+from UserDatabse import UserDatabase
 
+#Main will show us how we define each method and the result from each print statement
 class Main:
     InvData = InventoryDatabase()
+    UData = UserDatabase()
+    #Prints the column name and then the associated values in that row: 'itemID, 1003' as an Array
     print(InvData.csv_w_label[2:4])
     print("\n")
-    print(InvData.csv_w_label[2])
     
+    #Prints the same things as before but as a String
+    print(InvData.csv_w_label[2])
     print("\n")
 
+    #Prints the first header of the CSV file
     print(InvData.data_values_2D[0][0])
     print("\n")
+    
+    #Prints the first item under the first column
     print(InvData.data_values_2D[1][0])
     print("\n")
+    
+    #Prints the second header of the CSV file
     print(InvData.data_values_2D[0][1])
     print("\n")
+
+########################################## USER DATABASE TESTING ###########################################
+    print("Test For Users")
+    print("\n")
+    
+    #Prints the header of the CSV: "name"
+    print(UData.data_values_2D[0][0])
+    print("\n")
+    
+    #Prints the first name under the name column in the CSV: "Anthony Langer"
+    print(UData.data_values_2D[1][0])
+    print("\n")
+    
