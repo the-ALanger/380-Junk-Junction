f1913c83e000dc68b4082510b3ef0a29daaf359d
IanFlack22 <ian.flack.694@my.csun.edu>
2025-12-02 18:42:17 -0800

    Added test cases for two methods in InventoryDatabase.py

--- tests/test_InventoryDatabase.py
+++ tests/test_InventoryDatabase.py
@@ -1,6 +1,29 @@
 
 from InventoryDatabase import InventoryDatabase
+from UserCurrent import UserCurrent
 
-def test_create_new_item():
-    result = InventoryDatabase.create_new_item(
-        assert result 
+
+def test_get_item_with_id():
+    result = InventoryDatabase.get_item_with_id(1004)
+    assert result.itemName == "Smartphone"
+    
+   
+# def test_get_items_with_user():
+#     result = InventoryDatabase.get_items_with_user(77778)
+#     assert len(result) == 4
+#     assert result[0].itemName == "Smartphone"
+#     assert result[1].itemName == "Bookshelf"
+#     assert result[2].itemName == "Tennis Racket"
+#     assert result[3].itemName == "Coffee Table"
+    
+
+def test_get_items_with_user_id():
+    result = InventoryDatabase.get_items_with_user_id(1001)
+    assert result == [1]
+    
+
+def test_get_items_with_user_id():
+    result = InventoryDatabase.get_items_with_user_id(77777)
+    assert len(result) == 2
+    assert result[0].itemName == "Laptop"
+    assert result[1].itemName == "Desk Chair"
