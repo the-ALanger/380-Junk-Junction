5c27100790beff299bdb34598ddfcf41b0e7e6d3
IanFlack22 <ian.flack.694@my.csun.edu>
2025-12-05 15:07:43 -0800

    fixed typo in UserCurrent test case

--- tests/test_UserCurrent.py
+++ tests/test_UserCurrent.py
@@ -2,11 +2,11 @@ from UserCurrent import UserCurrent
 from UserDatabase import UserDatabase
 
 
-def test_check_of_user_exists():
+def test_check_if_user_exists():
     result = UserCurrent.check_if_user_exists("ian.flack.694@my.csun.edu", "password")
     assert result == UserDatabase.get_user_with_id(77777)
     
-def test_check_of_user_exists_by_email():
+def test_check_if_user_exists_by_email():
     result = UserCurrent.check_if_user_exists_by_email("ian.flack.694@my.csun.edu")
     assert result == UserDatabase.get_user_with_id(77777)
 
